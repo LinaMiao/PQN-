1ae3130d2a7450aacd9d293eed4688b8979a726a
lina <linaouc@gmail.com>
2012-08-15 12:38:54 -0700

    running with new functions, however, still  not a good way to compare two lasso problem, do not need to add another parameter, for comparing lasso proble, simply let them run for same iterations, with same optTol, if needed can be modified later

--- experiments/help_spgl1/modifying/task10strictvssparse/ex1.m
+++ experiments/help_spgl1/modifying/task10strictvssparse/ex1.m
@@ -1,15 +1,9 @@
-% this experiment is to test whether pqnl1 can work for the expqnle given
-% by help spgl1
+% if [Q,R] = qr(A',0);  A = Q';
 
 %% addpath for PQN working
-%addpath(genpath('/Volumes/Users/linamiao/Dropbox/PQN/'))
-cd ../../../../pqnl1;
+cd ../../../../functions;
 addpath(genpath(pwd))
 cd ../experiments/help_spgl1/modifying/task10strictvssparse
-
-%stream = RandStream.getGlobalStream;
-%reset(stream);
-
 
 %% sample matrix
 m = 120; n = 512; k = 20; % m rows, n cols, k nonzeros.
@@ -27,28 +21,47 @@ opts.nPrevVals = 1; % opt out the nonmonotone line search
 %% problem setting
 % strict problem setting
 p = randperm(n); x0 = zeros(n,1); x0(p(1:k)) = sign(randn(k,1));
-figure;plot(x0)
-b0  = A*x0 + 0.005 * randn(m,1);
+h = figure;title('sparse signal')
+plot(x0);
+saveas(h,'sparse signal');
+b0  = A*x0;
 
 % compressible problem setting
 nn = linspace(0,1,n);
 x0_compress = exp(-nn.^.1);
 x0_compress = x0_compress - min(x0_compress);
-figure;plot(x0_compress)
+h = figure;title('compress signal')
+plot(x0_compress)
+saveas(h,'compress signal')
 x0_compress = x0_compress(:);
 b_compress  = A*x0_compress + 0.005 * randn(m,1);
 
 %% reconstruct
-[x_sparse,r_sparse,g_sparse,info_sparse] = pqnl1_2(A, b0, 0, 1e-3, zeros(size(A,2),1), opts); % Find BP sol'n.
-[x_compress,r_compress,g_compress,info_compress] = pqnl1_2(A, b_compress, 0, 1e-3, zeros(size(A,2),1), opts); % Find BP sol'n.
-figure('Name','pqn'); 
-subplot(2,1,1);plot(x_sparse);subplot(2,1,2);plot(x_compress);
+tau = norm(x0,1);
+% sparse
+opts.fid = fopen('sparse_spg.txt','w');
+[x_spg1,r_spgl,g_spgl,info_spg1] = spgl1(A, b0, tau, [], zeros(size(A,2),1), opts);
+opts.fid = fopen('sparse_pqn.txt','w');
+opts.optTol = info_spg1.rNorm;
+[x_sparse,r_sparse,g_sparse,info_sparse] = pqnl1_2(A, b0, tau,[], zeros(size(A,2),1), opts); % Find BP sol'n.
 
+h = figure;title('sparse resconstruct'); 
+subplot(2,1,1);plot(x_spg1);title('spg');axis('tight')
+subplot(2,1,2);plot(x_sparse);title('pqn');axis('tight')
+saveas(h,'sparse resconstruct')
 
-[x_spg1,r_spgl,g_spgl,info_spg1] = spgl1(A, b0, 0, 1e-3, zeros(size(A,2),1), opts);
-[x_spg2,r_spg2,g_spg2,info_spg2] = spgl1(A, b_compress, 0, 1e-3, zeros(size(A,2),1), opts); % Find BP sol'n.
-figure('Name','spg'); 
-subplot(2,1,1);plot(x_spg1);subplot(2,1,2);plot(x_spg2);
+% compress
+tau = norm(x0_compress,1);
+opts.fid = fopen('compress_spg.txt','w');
+[x_spg2,r_spg2,g_spg2,info_spg2] = spgl1(A, b_compress, tau,[], zeros(size(A,2),1), opts); % Find BP sol'n.
+opts.fid = fopen('compress_pqn.txt','w');
+opts.optTol = info_spg2.rNorm;
+[x_compress,r_compress,g_compress,info_compress] = pqnl1_2(A, b_compress, tau, [], zeros(size(A,2),1), opts); % Find BP sol'n.
+
+h = figure; title('compress resconstruct'); 
+subplot(2,1,1);plot(x_spg2);title('spg');axis('tight')
+subplot(2,1,2);plot(x_compress);title('pqn');axis('tight')
+saveas(h,'compress resconstruct')
 
 
 %% show result
@@ -57,19 +70,21 @@ info_spg1
 info_compress
 info_spg2
 
-figure('Name','strict sparse Solution paths')
+h = figure;title('strict sparse Solution paths')
 plot(info_sparse.xNorm1,info_sparse.rNorm2,info_spg1.xNorm1,info_spg1.rNorm2);hold on
 scatter(info_sparse.xNorm1,info_sparse.rNorm2);
 scatter(info_spg1.xNorm1,info_spg1.rNorm2);hold off
 legend('pqn','spg')
 axis tight
+saveas(h,'strict sparse Solution paths')
 
-figure('Name','compress signal Solution paths')
+h = figure;title('compress signal Solution paths')
 plot(info_compress.xNorm1,info_compress.rNorm2,info_spg2.xNorm1,info_spg2.rNorm2);hold on
 scatter(info_compress.xNorm1,info_compress.rNorm2);
 scatter(info_spg2.xNorm1,info_spg2.rNorm2);hold off
 legend('pqn','spg')
 axis tight
+saveas(h,'compress signal Solution paths')
 
 
 
